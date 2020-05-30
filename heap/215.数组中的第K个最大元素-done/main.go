7a7bb272afb854c7bb21f52c15bb05ba745bc9cc
yinzhen <yz_pearl@163.com>
2020-05-30 16:39:26 +0800

    heap

--- heap/215.数组中的第K个最大元素-done/main.go
+++ heap/215.数组中的第K个最大元素-done/main.go
@@ -5,64 +5,18 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/yinzhenzhen/learning-leetcode/types"
+)
 
-type SqList struct {
-	data []int
-	length int
-}
+
 
 func findKthLargest(nums []int, k int) int {
-	newNume := []int{0}
-	newNume = append(newNume, nums...)
-	L := &SqList{newNume, len(nums)}
-	HeapSort(L)
-	fmt.Println(L.data)
-	return L.data[L.length-k+1]
-}
-
-// 堆排序
-func HeapSort(L *SqList) {
-	// i 为前一半元素的索引，这些元素都有左右子节点，这些元素为关键字
-	for i := L.length/2; i > 0; i-- {
-		HeapAjust(L, i, L.length)
-	}
-
-	// 输出大顶堆得到升序结果
-	for i := L.length; i > 1; i-- {
-		swap(L, 1, i)
-		HeapAjust(L, 1, i-1)
-	}
-
-}
-
-func swap(L *SqList, m int, n int)  {
-	temp := L.data[m]
-	L.data[m] = L.data[n]
-	L.data[n] = temp
-}
-
-// 调整顺序，使s->m的元素成为大顶堆
-func HeapAjust(L *SqList, s int, m int)  {
-	var temp, j int
-	temp = L.data[s]
-	// 沿关键字较大的孩子节点向下筛选
-	for j = 2*s; j <= m; j*=2 {
-		// s=1 j=2 比较1的子节点2、3的大小，如果3比2大，继续找3的子节点6、7比较他们的大小找到更大的
-		if j < m && L.data[j] < L.data[j+1] {
-			j++
-		}
-		// 如果1比2和3都大，直接跳出循环
-		if temp >= L.data[j] {
-			break
-		}
-		// 将大的数赋值给1所在的位置
-		L.data[s] = L.data[j]
-		// 数大的索引赋值给s
-		s = j
-	}
-	// 将1赋值给大的数所在的位置
-	L.data[s] = temp
+	L := types.ConstructHeapSqList(nums)
+	L.HeapSortAsc()
+	fmt.Println(L.Data)
+	return L.Data[L.Length-k+1]
 }
 
 func main()  {
