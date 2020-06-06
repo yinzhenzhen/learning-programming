54666599e59402883cea1f75973852ea97537934
yinzhen <yz_pearl@163.com>
2020-06-06 14:19:22 +0800

    tree

---
+++ brainteaser/1227.飞机座位分配概率/main.go
@@ -0,0 +1,18 @@
+/**
+ * @Time : 2020-06-05 17:12
+ * @Author : yz
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func nthPersonGetsNthSeat(n int) float64 {
+
+}
+
+func main()  {
+	fmt.Println(nthPersonGetsNthSeat(3))
+}
--- types/tree.go
+++ types/tree.go
@@ -5,16 +5,57 @@
 
 package types
 
+import "fmt"
+
+// 二叉树
 type TreeNode struct {
-	Val interface{}
+	Val int
 	Left *TreeNode
 	Right *TreeNode
 }
 
-func ConstructTree(elems []interface{}) *TreeNode {
-	var root TreeNode
+func (tree *TreeNode) ConstructTree(elems []int, leftIndex int, rightIndex int)  {
+	if leftIndex < len(elems) {
+		left := &TreeNode{}
+		left.Val = elems[leftIndex]
+		tree.Left = left
+		tree.Left.ConstructTree(elems, 2*leftIndex+1, 2*leftIndex+2)
+	}
+	if rightIndex < len(elems) {
+		right := &TreeNode{}
+		right.Val = elems[rightIndex]
+		tree.Right = right
+		tree.Right.ConstructTree(elems, 2*rightIndex+1, 2*rightIndex+2)
+	}
+	return
+}
 
+// 前序遍历
+func (tree *TreeNode) PreOrderTraverse()  {
+	if tree == nil {
+		return
+	}
+	fmt.Printf("%d\n", tree.Val)
+	tree.Left.PreOrderTraverse()
+	tree.Right.PreOrderTraverse()
+}
 
+// 中序遍历
+func (tree *TreeNode) InOrderTraverse()  {
+	if tree == nil {
+		return
+	}
+	tree.Left.InOrderTraverse()
+	fmt.Printf("%d\n", tree.Val)
+	tree.Right.InOrderTraverse()
+}
 
-	return &root
+// 后序遍历
+func (tree *TreeNode) PostOrderTraverse()  {
+	if tree == nil {
+		return
+	}
+	tree.Left.PostOrderTraverse()
+	tree.Right.PostOrderTraverse()
+	fmt.Printf("%d\n", tree.Val)
 }
