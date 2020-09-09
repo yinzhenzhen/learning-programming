ec8f5829cbf032e711d3fc00d1983e392b363762
yinzhen <yz_pearl@163.com>
2020-09-09 09:26:40 +0800

    others

--- brainteaser/1227.飞机座位分配概率/main.go
+++ brainteaser/1227.飞机座位分配概率/main.go
@@ -10,9 +10,16 @@ import (
 )
 
 func nthPersonGetsNthSeat(n int) float64 {
-
+	return 0.0
 }
 
 func main()  {
-	fmt.Println(nthPersonGetsNthSeat(3))
+	//fmt.Println(nthPersonGetsNthSeat(3))
+	s := []int{1, 3, 5, 7}
+	for i, v := range s  {
+		if v == 7 {
+			s = append(s[:i], s[i+1:]...)
+		}
+	}
+	fmt.Println(s)
 }
--- stack/880.索引处的解码字符串/main.go
+++ stack/880.索引处的解码字符串/main.go
@@ -7,9 +7,7 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"strconv"
-	"unsafe"
 )
 
 func decodeAtIndex(S string, K int) string {
@@ -44,89 +42,5 @@ func decodeAtIndex(S string, K int) string {
 }
 
 func main()  {
-	//fmt.Println(decodeAtIndex("y959q969u3hb22odq595",222280369))
-
-	demo := Demo{Name:"22", Addr:"33", School:"44"}
-
-	fmt.Println(demo.Age)
-
-	//setByTags(demo)
-	setByPoint(demo)
+	fmt.Println(decodeAtIndex("y959q969u3hb22odq595",222280369))
 }
-
-// ------------------------------------------------------------------------------
-
-type Demo struct {
-	Age int `json:"Age"`
-	Name string `json:"Name"`
-	Addr string `json:"Addr"`
-	School string `json:"School"`
-}
-
-// 通过反射改变值
-func setByTags(demo Demo) {
-	fmt.Println("before : ", demo)
-
-	// 设置结构体值需要获取的
-	s := reflect.ValueOf(&demo).Elem()
-
-	// 类型
-	t := reflect.TypeOf(demo)
-
-	// 字段数量
-	num := t.NumField()
-
-	for i := 0; i < num; i++ {
-		f := t.Field(i)
-		tagName := f.Tag.Get("json")
-		fType := f.Type.Kind()
-
-		switch fType {
-		case reflect.Int:
-			s.FieldByName(tagName).SetInt(11111)
-		case reflect.String:
-			s.FieldByName(tagName).SetString("yy")
-		}
-
-		fmt.Printf("%d: %s %s = %v\n", i, f.Name, fType.String(), s.FieldByName(tagName).String())
-	}
-
-	fmt.Println("after : ", demo)
-}
-
-// 通过指针改变值
-func setByPoint(demo Demo)  {
-	fmt.Println("before : ", demo)
-
-	// 结构体起始位置
-	demoPointer := unsafe.Pointer(&demo)
-
-	// 类型
-	t := reflect.TypeOf(demo)
-
-	// 字段数量
-	num := t.NumField()
-
-	for i := 0; i < num; i++ {
-		f := t.Field(i)
-		fType := f.Type.Kind()
-
-		fmt.Println(f.Index)
-
-		// 偏移量
-		offset := f.Offset
-
-		switch fType {
-		case reflect.Int:
-			fieldPointer := (*int)(unsafe.Pointer(uintptr(demoPointer) + offset))
-			*fieldPointer = 22222
-		case reflect.String:
-			fieldPointer := (*string)(unsafe.Pointer(uintptr(demoPointer) + offset))
-			*fieldPointer = "zz"
-		}
-		fmt.Printf("%d: %s %s = %v\n", i, f.Name, fType.String(), offset)
-	}
-
-	fmt.Println("after : ", demo)
-}
-
