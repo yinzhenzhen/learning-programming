5ec0d9a63111f5f2c8b266aa67aeea92a9fab569
yinzhen <yz_pearl@163.com>
2020-09-16 01:06:17 +0800

    more

---
+++ nowcoder/huawei/HJ8-合并表记录/main.go
@@ -0,0 +1,45 @@
+/**
+ * @Time : 2020-09-14 23:24
+ * @Author : yz
+ */
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main()  {
+
+	var num int
+
+	for {
+		m := make(map[int]int)
+		s := []int{}
+		n, _ := fmt.Scan(&num)
+		if n == 0 {
+			break
+		}
+
+		for i := 0; i < num; i++  {
+
+			var tempIndex, tempData int
+			_, _ = fmt.Scan(&tempIndex, &tempData)
+
+			if _, ok := m[tempIndex]; ok {
+				m[tempIndex] = m[tempIndex] + tempData
+			} else {
+				m[tempIndex] = tempData
+				s = append(s, tempIndex)
+			}
+		}
+
+		sort.Ints(s)
+
+		for i := 0; i < len(s); i++ {
+			fmt.Println(s[i], m[s[i]])
+		}
+	}
+
+}
---
+++ nowcoder/huawei/T1-字符串变换最小字符串/main.go
@@ -0,0 +1,74 @@
+/**
+ * @Time : 2020-09-15 19:10
+ * @Author : yz
+ */
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func minStr(str string) (result string) {
+
+	// 用来进行对比
+	data := str
+
+	// 记录之前试图能替换其他元素的元素
+	// 被替换元素的位置
+	var indexA = -1
+	// 替换元素的位置
+	var indexB = -1
+
+	for i := 1; i < len(str); i++ {
+
+		for j := 0; j < i; j++  {
+
+			// 如果元数据的当前元素比data小，记录
+			if str[i] < data[j] {
+
+				// 如果之前存在期望替换的元素
+				if indexA != -1 && indexB != -1 {
+					// 判断新的元素是否比之前的元素还小
+					if str[i] < str[indexB] {
+						indexB = i
+					}
+				} else {
+					indexA = j
+					indexB = i
+					break
+				}
+
+			}
+		}
+
+	}
+
+	// 需要进行元素替换
+	if indexA != -1 && indexB != -1 {
+		temp := strings.Split(str, "")
+		temp[indexA] = string(str[indexB])
+		temp[indexB] = string(str[indexA])
+		result = strings.Join(temp, "")
+	} else {
+		result = str
+	}
+
+	return
+}
+
+func main() {
+	var str string
+
+	for {
+		n, _ := fmt.Scan(&str)
+		if n == 0 {
+			break
+		}
+
+		fmt.Println(minStr(str))
+	}
+
+	//fmt.Println(minStr("bcdefa"))
+}
