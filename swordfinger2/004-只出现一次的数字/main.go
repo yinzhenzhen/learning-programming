0a1d015cf33d11455c4cae8be1199021c76076f4
yinzhen <yinzhen@rockontrol.com>
2022-04-12 16:22:58 +0800

    004

---
+++ swordfinger2/004-只出现一次的数字/main.go
@@ -0,0 +1,36 @@
+/**
+ * @Time : 4/12/22 3:36 PM
+ * @Author : yz
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(singleNumber([]int{0, 1, 0, 1, 0, 1, 100}))
+}
+
+func singleNumber(nums []int) int {
+
+	m := make(map[int]int)
+	result := 0
+
+	for i := 0; i < len(nums); i++ {
+		_, ok := m[nums[i]]
+		if !ok {
+			m[nums[i]] = 1
+		} else {
+			m[nums[i]]++
+		}
+	}
+
+	for k, v := range m {
+		if v == 1 {
+			result = k
+			break
+		}
+	}
+
+	return result
+}
