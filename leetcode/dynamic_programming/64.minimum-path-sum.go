c166d07896cba7e536536150f920c4a1ada53a11
zhenyin1011 <zhenyin1011@gmail.com>
2024-10-24 20:19:46 +0800

    添加标签/难度/代码注释

--- leetcode/dynamic_programming/64.minimum-path-sum.go
+++ leetcode/dynamic_programming/64.minimum-path-sum.go
@@ -5,19 +5,30 @@
 
 package dynamic_programming
 
-// f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j]
+/**
+标签：数组、动态规划、矩阵
+难度：中等
 
-//       j
-//	  i	   0  1  2
-//		 0 1  3  1
-//		 1 1  5  1
-//		 2 4  2  1
+题目内容：
+64.最小路径和
+给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+说明：每次只能向下或者向右移动一步。
 
-//       j
-//	  i	   0  1  2
-//		 0 1  4  5
-//		 1 1  6  6
-//		 2 5  7  7
+解题思路：见代码注释及状态方程
+f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j]
+
+         j
+	  i	   0  1  2
+		 0 1  3  1
+		 1 1  5  1
+		 2 4  2  1
+
+         j
+	  i	   0  1  2
+		 0 1  4  5
+		 1 1  6  6
+		 2 5  7  7
+*/
 
 func minPathSum(grid [][]int) int {
 
@@ -27,8 +38,8 @@ func minPathSum(grid [][]int) int {
 	// 列数
 	n := len(grid[0])
 
+	// 初始化二维数组
 	var f [][]int
-
 	for i := 0; i < m; i++ {
 		temp := make([]int, len(grid[i]))
 		f = append(f, temp)
