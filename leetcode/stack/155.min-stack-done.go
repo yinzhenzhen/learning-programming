8bf25142676a4cab8eb8e2f210a8f1608b92fd32
yinzhen <yz_pearl@163.com>
2024-10-25 15:20:38 +0800

    修改155

--- leetcode/stack/155.min-stack-done.go
+++ leetcode/stack/155.min-stack-done.go
@@ -5,33 +5,61 @@
 
 package stack
 
+import "math"
+
+/**
+标签：栈、设计
+难度：中等
+
+题目内容：
+155.最小栈：
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+实现 MinStack 类:
+	MinStack() 初始化堆栈对象。
+	void push(int val) 将元素val推入堆栈。
+	void pop() 删除堆栈顶部的元素。
+	int top() 获取堆栈顶部的元素。
+	int getMin() 获取堆栈中的最小元素。
+
+解题思路：见代码注释
+*/
+
 type MinStack struct {
-	Elem []int
+	stack    []int
+	minStack []int
 }
 
 /** initialize your data structure here. */
 func NewMinStack() MinStack {
-	return MinStack{}
+	return MinStack{
+		stack:    []int{},
+		minStack: []int{math.MaxInt64},
+	}
 }
 
 func (this *MinStack) Push(x int) {
-	this.Elem = append(this.Elem, x)
+	this.stack = append(this.stack, x)
+	// 如果新元素比最小栈的栈顶元素要小，则添加x到minStack，否则添加栈顶元素到minStack（最小元素未发生变化）
+	top := this.minStack[len(this.minStack)-1]
+	this.minStack = append(this.minStack, min(x, top))
 }
 
 func (this *MinStack) Pop() {
-	this.Elem = this.Elem[0 : len(this.Elem)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
 }
 
 func (this *MinStack) Top() int {
-	return this.Elem[len(this.Elem)-1]
+	return this.stack[len(this.stack)-1]
 }
 
 func (this *MinStack) GetMin() int {
-	var min = this.Elem[0]
-	for i := 1; i < len(this.Elem); i++ {
-		if this.Elem[i] < min {
-			min = this.Elem[i]
-		}
+	return this.minStack[len(this.minStack)-1]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
 	}
-	return min
+	return y
 }
