8d98b97042e1f8c51b8842521d7da79b9a94b48a
yinzhen <yz_pearl@163.com>
2024-11-07 14:21:08 +0800

    修改215

--- leetcode/heap/215.kth-largest-element-in-an-array.go
+++ leetcode/heap/215.kth-largest-element-in-an-array.go
@@ -10,9 +10,23 @@ import (
 	"github.com/yinzhenzhen/learning-programming/types"
 )
 
+/**
+标签：数组、分治、快速选择、排序、堆（优先队列）
+难度：中等
+
+题目内容：
+215.数组中的第K个最大元素：
+给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+
+解题思路：
+
+*/
+
 func findKthLargest(nums []int, k int) int {
-	L := types.ConstructHeapSqList(nums)
-	L.HeapSortAsc()
-	fmt.Println(L.Data)
-	return L.Data[L.Length-k]
+	l := types.ConstructHeapSqList(nums)
+	l.HeapSortAsc()
+	fmt.Println(l.Data)
+	return l.Data[l.Length-k]
 }
--- types/heap.go
+++ types/heap.go
@@ -11,102 +11,100 @@ type HeapSqList struct {
 }
 
 func ConstructHeapSqList(nums []int) *HeapSqList {
-	newNums := []int{}
-	newNums = append(newNums, nums...)
-	L := &HeapSqList{newNums, len(nums)}
-	return L
+	l := &HeapSqList{nums, len(nums)}
+	return l
 }
 
 // 堆排序-升序排序
-func (L *HeapSqList) HeapSortAsc() {
+func (l *HeapSqList) HeapSortAsc() {
 	// i 为前一半元素的索引，这些元素都有左右子节点，这些元素为关键字
-	for i := L.Length/2 - 1; i >= 0; i-- {
-		MaxHeapAjust(L, i, L.Length)
+	for i := l.Length/2 - 1; i >= 0; i-- {
+		MaxHeapAjust(l, i, l.Length)
 	}
 
 	// 输出大顶堆得到升序结果
-	for i := L.Length - 1; i > 0; i-- {
-		swap(L, 0, i)
-		MaxHeapAjust(L, 0, i)
+	for i := l.Length - 1; i > 0; i-- {
+		swap(l, 0, i)
+		MaxHeapAjust(l, 0, i)
 	}
 }
 
-func swap(L *HeapSqList, m int, n int) {
-	temp := L.Data[m]
-	L.Data[m] = L.Data[n]
-	L.Data[n] = temp
-}
-
 // 调整顺序，使s->m的元素成为大顶堆
-func MaxHeapAjust(L *HeapSqList, s int, length int) {
+func MaxHeapAjust(l *HeapSqList, s int, length int) {
 	var temp, j int
-	temp = L.Data[s]
+	temp = l.Data[s]
 	// 沿关键字较大的孩子节点向下筛选
 	for j = 2*s + 1; j < length; j = j*2 + 1 {
 		// s=0 j=1 比较0的子节点1、2的大小，如果2比1大，继续找2的子节点5、6比较他们的大小找到更大的
-		if j+1 < length && L.Data[j] < L.Data[j+1] {
+		if j+1 < length && l.Data[j] < l.Data[j+1] {
 			j++
 		}
 		// 如果0比1和2都大，直接跳出循环
-		if temp >= L.Data[j] {
+		if temp >= l.Data[j] {
 			break
 		}
 		// 将大的数赋值给0所在的位置
-		L.Data[s] = L.Data[j]
+		l.Data[s] = l.Data[j]
 		// 数大的索引赋值给s=0
 		s = j
 	}
 	// 将0赋值给大的数所在的位置
-	L.Data[s] = temp
+	l.Data[s] = temp
 }
 
 // 大顶堆
-func (L *HeapSqList) MaxHeap() {
-	for i := L.Length/2 - 1; i > 0; i-- {
-		MaxHeapAjust(L, i, L.Length)
+func (l *HeapSqList) MaxHeap() {
+	for i := l.Length/2 - 1; i > 0; i-- {
+		MaxHeapAjust(l, i, l.Length)
 	}
 }
 
 // 堆排序-降序排序
-func (L *HeapSqList) HeapSortDesc() {
+func (l *HeapSqList) HeapSortDesc() {
 	// i 为前一半元素的索引，这些元素都有左右子节点，这些元素为关键字
-	for i := L.Length/2 - 1; i >= 0; i-- {
-		MinHeapAjust(L, i, L.Length)
+	for i := l.Length/2 - 1; i >= 0; i-- {
+		MinHeapAjust(l, i, l.Length)
 	}
 
 	// 输出大顶堆得到升序结果
-	for i := L.Length - 1; i > 0; i-- {
-		swap(L, 0, i)
-		MinHeapAjust(L, 0, i)
-	}
-}
-
-// 小顶堆
-func (L *HeapSqList) MinHeap() {
-	for i := L.Length/2 - 1; i > 0; i-- {
-		MinHeapAjust(L, i, L.Length)
+	for i := l.Length - 1; i > 0; i-- {
+		swap(l, 0, i)
+		MinHeapAjust(l, 0, i)
 	}
 }
 
 // 调整顺序，使s->m的元素成为小顶堆
-func MinHeapAjust(L *HeapSqList, s int, length int) {
+func MinHeapAjust(l *HeapSqList, s int, length int) {
 	var temp, j int
-	temp = L.Data[s]
+	temp = l.Data[s]
 	// 沿关键字较大的孩子节点向下筛选
 	for j = 2*s + 1; j < length; j = j*2 + 1 {
 		// s=0 j=1 比较0的子节点1、2的大小，如果2比1大，继续找2的子节点5、6比较他们的大小找到更大的
-		if j+1 < length && L.Data[j] > L.Data[j+1] {
+		if j+1 < length && l.Data[j] > l.Data[j+1] {
 			j++
 		}
 		// 如果0比1和2都小，直接跳出循环
-		if temp <= L.Data[j] {
+		if temp <= l.Data[j] {
 			break
 		}
 		// 将小的数赋值给0所在的位置
-		L.Data[s] = L.Data[j]
+		l.Data[s] = l.Data[j]
 		// 数小的索引赋值给s
 		s = j
 	}
 	// 将0赋值给小的数所在的位置
-	L.Data[s] = temp
+	l.Data[s] = temp
+}
+
+// 小顶堆
+func (l *HeapSqList) MinHeap() {
+	for i := l.Length/2 - 1; i > 0; i-- {
+		MinHeapAjust(l, i, l.Length)
+	}
+}
+
+func swap(l *HeapSqList, m int, n int) {
+	temp := l.Data[m]
+	l.Data[m] = l.Data[n]
+	l.Data[n] = temp
 }
